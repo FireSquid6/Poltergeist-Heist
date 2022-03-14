7e4deac9c4f7a48877df866d5fa7cba965c38870
Jonathan Deiss <39541470+FireSquid6@users.noreply.github.com>
2022-03-14 15:35:45 -0500

    start making a level

--- src/instances/level_entities/crate/crate.gd
+++ src/instances/level_entities/crate/crate.gd
@@ -2,10 +2,23 @@ extends KinematicBody2D
 class_name Crate
 
 export var heavy = false
+export var GRV = 30
 onready var pushable = true
+onready var velocity = Vector2(0, 0)
 
 func push(velocity: Vector2) -> void:
 	move_and_slide(velocity, Vector2())
+
+
+func _physics_process(delta):
+	var on_floor = $RayCast2D.is_colliding() or $RayCast2D2.is_colliding()
+	if !on_floor:
+		velocity.y += GRV
+	else:
+		velocity.y = 0
+	
+	move_and_slide(velocity)
+	$Label.text = str(velocity)
 
 
 func _process(_delta):
--- src/instances/level_entities/toggleables/door/door.gd
+++ src/instances/level_entities/toggleables/door/door.gd
@@ -1,5 +1,10 @@
 extends Toggleable
 
+export var start_open = false
+
+func _ready():
+	if start_open:
+		turn_on()
 
 func turn_on():
 	$CollisionShape2D.set_deferred("disabled", true)
